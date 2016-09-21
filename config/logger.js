ba6391f5bf871361df961b3b6aa460a764388615
Chun-Kai Wang <chunkai1312@gmail.com>
2016-09-22 02:07:01 +0800

    refactor: logger

--- config/logger.js
+++ config/logger.js
@@ -6,33 +6,38 @@ const DailyRotateFile = require('winston-daily-rotate-file')
 const config = require('../config')
 
 const logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true,
-      prettyPrint: true
-    }),
-    new DailyRotateFile({
-      name: 'log-file',
-      level: 'info',
-      handleExceptions: true,
-      json: true,
-      colorize: false,
-      filename: path.join(config.root, 'logs', 'log'),
-      datePattern: '_yyyyMMdd.json'
-    }),
-    new DailyRotateFile({
-      name: 'error-file',
-      level: 'error',
-      handleExceptions: true,
-      json: true,
-      colorize: false,
-      filename: path.join(config.root, 'logs', 'error'),
-      datePattern: '_yyyyMMdd.json'
-    })
-  ],
+  transports: (config.env === 'development')
+    ? [
+      new winston.transports.Console({
+        level: 'debug',
+        colorize: true,
+        prettyPrint: true,
+        handleExceptions: true
+      })
+    ]
+    : [
+      new winston.transports.Console({
+        level: 'verbose',
+        colorize: true,
+        prettyPrint: true,
+        handleExceptions: true
+      }),
+      new DailyRotateFile({
+        name: 'log-file',
+        level: 'info',
+        json: true,
+        filename: path.join(config.root, 'logs', 'log'),
+        datePattern: '-yyyyMMdd.log'
+      }),
+      new DailyRotateFile({
+        name: 'error-file',
+        level: 'error',
+        json: true,
+        handleExceptions: true,
+        filename: path.join(config.root, 'logs', 'error'),
+        datePattern: '-yyyyMMdd.log'
+      })
+    ],
   exitOnError: false
 })
 
