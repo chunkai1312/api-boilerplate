d746153403f695c1cbad371fabff5dc9b73344c4
Chun-Kai Wang <chunkai1312@gmail.com>
2016-09-24 22:43:25 +0800

    chore: comments

--- config/local.js
+++ config/local.js
@@ -3,17 +3,8 @@
 const path = require('path')
 
 module.exports = {
-
-  // Environment Variable, default = development
-  env: process.env.NODE_ENV || 'development',
-
-  // Server Port
-  port: process.env.PORT || 8080,
-
-  // Server IP
-  ip: process.env.IP || '127.0.0.1',
-
-  // Root path of server
-  root: path.normalize(`${__dirname}/..`)
-
+  env: process.env.NODE_ENV || 'development',  // environment variable, default = development
+  port: process.env.PORT || 8080,              // server port
+  ip: process.env.IP || '127.0.0.1',           // server ip
+  root: path.normalize(`${__dirname}/..`)      // root path
 }
