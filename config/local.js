78269270d409f31515601d827bc94c9b37c44d85
Chun-Kai Wang <chunkai1312@gmail.com>
2016-09-27 16:23:13 +0800

    refactor: remove ip

--- config/local.js
+++ config/local.js
@@ -5,6 +5,5 @@ const path = require('path')
 module.exports = {
   env: process.env.NODE_ENV || 'development',  // environment variable, default = development
   port: process.env.PORT || 8080,              // server port
-  ip: process.env.IP || '127.0.0.1',           // server ip
   root: path.normalize(`${__dirname}/..`)      // root path
 }
