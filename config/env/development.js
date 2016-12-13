08121527d2c4dc063ad03093d7a4c4accc3b1a0e
Chun-Kai Wang <chunkai1312@gmail.com>
2016-12-13 17:00:38 +0800

    refactor(env): mongodb

--- config/env/development.js
+++ config/env/development.js
@@ -3,6 +3,8 @@
 module.exports = {
   mongoDB: {
     uri: 'mongodb://localhost:27017/api-seed-dev',
-    options: {}
+    options: {
+      server: { socketOptions: { keepAlive: 1 } }
+    }
   }
 }
--- config/env/production.js
+++ config/env/production.js
@@ -3,6 +3,8 @@
 module.exports = {
   mongoDB: {
     uri: 'mongodb://localhost:27017/api-seed',
-    options: {}
+    options: {
+      server: { socketOptions: { keepAlive: 1 } }
+    }
   }
 }
