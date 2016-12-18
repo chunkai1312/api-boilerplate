3e99006b4d38f909c271138caa4d84bc094c0a5d
Chun-Kai Wang <chunkai1312@gmail.com>
2016-12-19 01:10:26 +0800

    feat(middlewares): res.created()

--- api/middlewares/response_hook.js
+++ api/middlewares/response_hook.js
@@ -2,12 +2,22 @@
 
 module.exports = () => (req, res, next) => {
   res.ok = sendOK
+  res.created = sendCreated
   next()
 }
 
 function sendOK (data) {
   const res = this
   res.status(200)
+
+  if (!data) return res.send()
+  if (typeof data !== 'object') return res.send(data)
+  return res.json(data)
+}
+
+function sendCreated (data) {
+  const res = this
+  res.status(201)
 
   if (!data) return res.send()
   if (typeof data !== 'object') return res.send(data)
