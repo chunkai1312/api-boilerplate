26067250fca3bc14f65bba4ed79c2d3469f79897
Chun-Kai Wang <chunkai1312@gmail.com>
2017-01-16 01:06:22 +0800

    test(*): use supertest

--- test/thing.test.js
+++ test/thing.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const expect = require('chai').expect
-const request = require('supertest-as-promised')
+const request = require('supertest')
 const app = require('../app')
 
 describe('Thing API:', () => {
