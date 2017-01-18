86df6310a91c9ad010d9bb940b8f465a649b1bde
Chun-Kai Wang <chunkai1312@gmail.com>
2017-01-18 17:33:59 +0800

    feat(middlewares): error logger

--- src/middlewares/errorhandler.js
+++ src/middlewares/errorhandler.js
@@ -1,3 +1,18 @@
 import apiErrorHandler from 'api-error-handler'
+import expressWinston from 'express-winston'
+import { errors } from 'compose-middleware'
+import config from '../../config'
+import winstonInstance from '../../config/logger'
 
-export default () => apiErrorHandler
+const errorLogger = (config.env === 'development')
+  ? () => (err, req, res, next) => winstonInstance.error(err) || next(err)
+  : () => expressWinston.errorLogger({ winstonInstance })
+
+const handlers = [
+  errorLogger(),
+  apiErrorHandler()
+]
+
+if (config.env === 'test') handlers.shift()
+
+export default () => errors(handlers)
