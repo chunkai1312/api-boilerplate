8a9e911e368562475339eaa8e60582c352608b6d
Chun-Kai Wang <chunkai1312@gmail.com>
2017-01-18 17:00:35 +0800

    refactor(*): index.js

--- index.js
+++ index.js
@@ -2,19 +2,12 @@
 
 const http = require('http')
 const config = require('./config')
+const logger = require('./config/logger')
 
 const app = (config.env === 'production')
   ? require('./dist/app').default
   : require('babel-register') && require('./src/app').default
 
 http.createServer(app).listen(config.port, () => {
-  console.log('Express server listening on %d, in %s mode', config.port, config.env)
+  logger.verbose('Express server listening on %d, in %s mode', config.port, config.env)
 })
-
-// require('babel-polyfill')
-// if (env === 'development') {
-//   require('babel-register')
-//   require('./src/app')
-// } else {
-//   require('./dist/app')
-// }
