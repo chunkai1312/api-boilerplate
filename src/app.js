3967f98b7a60ac5323b1b01cb3f868b31c50cd24
Chun-Kai Wang <chunkai1312@gmail.com>
2017-01-19 05:15:30 +0800

    fix(*): config path

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import 'babel-polyfill'
 import express from 'express'
 import middlewares, { errorHandler } from './middlewares'
 import routes from './routes'
-
-require('../config/mongoose')()
+import connectMongoDB from './config/mongoose'
 
 const app = express()
 
 app.use(middlewares())
 app.use(routes())
 app.use(errorHandler())
+
+connectMongoDB()
 
 export default app
