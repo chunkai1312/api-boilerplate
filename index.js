059317087fcf882f1a35e978769995162ec8c545
Chun-Kai Wang <chunkai1312@gmail.com>
2017-01-18 17:04:26 +0800

    refactor(*): index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const http = require('http')
 const config = require('./config')
 const logger = require('./config/logger')
 
-const app = (config.env === 'production')
-  ? require('./dist/app').default
-  : require('babel-register') && require('./src/app').default
+const app = (config.env === 'development')
+  ? require('babel-register') && require('./src/app').default
+  : require('./dist/app').default
 
 http.createServer(app).listen(config.port, () => {
   logger.verbose('Express server listening on %d, in %s mode', config.port, config.env)
