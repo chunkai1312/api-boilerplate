3d7e3609577fb2b0e966dad06d60a25b4e4aaed2
Chun-Kai Wang <chunkai1312@gmail.com>
2017-07-14 15:29:31 +0800

    refactor(controllers): todo

--- src/controllers/index.js
+++
@@ -1,1 +0,0 @@
-export * as todo from './todo'
--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,30 +1,58 @@
 import error from 'http-errors'
 import { Todo } from '../models'
 
-export async function index (req, res) {
-  const todos = await Todo.find()
-  res.status(200).json(todos)
+function TodoController (dependencies = {}) {
+  const todoController = {}
+
+  /**
+   * GET /api/todos
+   * Find all todo.
+   */
+  todoController.index = async (req, res) => {
+    const todos = await Todo.find()
+    res.status(200).json(todos)
+  }
+
+  /**
+   * POST /api/todos
+   * Create a new todo.
+   */
+  todoController.create = async (req, res) => {
+    const todo = await Todo.create(req.body)
+    res.status(201).json(todo)
+  }
+
+  /**
+   * GET /api/todos/:id
+   * Find one todo by ID.
+   */
+  todoController.show = async (req, res) => {
+    const todo = await Todo.findById(req.params.id)
+    if (!todo) throw error(404)
+    res.status(200).json(todo)
+  }
+
+  /**
+   * PUT /api/todos/:id
+   * Update an existing todo by ID.
+   */
+  todoController.update = async (req, res) => {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (!todo) throw error(404)
+    res.status(200).json(todo)
+  }
+
+  /**
+   * DELETE /api/todos/:id
+   * Destroy an existing todo by ID.
+   */
+  todoController.index = async (req, res) => {
+    const todo = await Todo.findByIdAndRemove(req.params.id)
+    if (!todo) throw error(404)
+    res.status(204).end()
+  }
+
+  return todoController
 }
 
-export async function create (req, res) {
-  const todo = await Todo.create(req.body)
-  res.status(201).json(todo)
-}
-
-export async function show (req, res) {
-  const todo = await Todo.findById(req.params.id)
-  if (!todo) throw error(404)
-  res.status(200).json(todo)
-}
-
-export async function update (req, res) {
-  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true })
-  if (!todo) throw error(404)
-  res.status(200).json(todo)
-}
-
-export async function destroy (req, res) {
-  const todo = await Todo.findByIdAndRemove(req.params.id)
-  if (!todo) throw error(404)
-  res.status(204).end()
-}
+export default TodoController
