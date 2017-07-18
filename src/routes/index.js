b20db405c9e987faebac9afe36398ae740b52cbd
Chun-Kai Wang <chunkai1312@gmail.com>
2017-07-18 16:39:35 +0800

    refactor: add wrap for routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 import { Router } from 'express'
 import TodoController from '../controllers/todo'
 
+const wrap = fn => (...args) => fn(...args).catch(args[2])
 const todoController = TodoController()
 
 const router = Router()
 
-  .get('/api/todos', todoController.index)
-  .post('/api/todos', todoController.create)
-  .get('/api/todos/:id', todoController.show)
-  .put('/api/todos/:id', todoController.update)
-  .delete('/api/todos/:id', todoController.destroy)
+  .get('/api/todos', wrap(todoController.index))
+  .post('/api/todos', wrap(todoController.create))
+  .get('/api/todos/:id', wrap(todoController.show))
+  .put('/api/todos/:id', wrap(todoController.update))
+  .delete('/api/todos/:id', wrap(todoController.destroy))
 
 export default () => router
