d651601ae8c06c42a4d2595ce762d7a62f87347f
Chun-Kai Wang <chunkai1312@gmail.com>
2017-07-21 18:33:44 +0800

    refactor: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 import TodoController from '../controllers/TodoController'
+import wrap from '../helpers/wrap'
 
-const wrap = fn => (...args) => fn(...args).catch(args[2])
 const todoController = TodoController()
 
 const router = Router()
 
-  .get('/api/todos', wrap(todoController.index))
-  .post('/api/todos', wrap(todoController.create))
-  .get('/api/todos/:id', wrap(todoController.show))
-  .put('/api/todos/:id', wrap(todoController.update))
-  .delete('/api/todos/:id', wrap(todoController.destroy))
+router.get('/api/todos', wrap(todoController.index))
+router.post('/api/todos', wrap(todoController.create))
+router.get('/api/todos/:id', wrap(todoController.show))
+router.put('/api/todos/:id', wrap(todoController.update))
+router.delete('/api/todos/:id', wrap(todoController.destroy))
 
 export default () => router
