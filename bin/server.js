dc7ed6be314a85225c3c9c67b8aef8a982180e5c
Chun-Kai Wang <chunkai1312@gmail.com>
2017-07-21 18:32:22 +0800

    refactor(bin): server.js

--- bin/server.js
+++ bin/server.js
@@ -7,8 +7,8 @@ var host = process.env.HOST || '0.0.0.0'
 var port = process.env.PORT || 8080
 
 var server = (env === 'development')
-  ? require('babel-register') && require('../src/app')
-  : require('../dist/app')
+  ? require('babel-register') && require('../src/app').default
+  : require('../dist/app').default
 
 server.listen(port, host, function () {
   console.log('Server is running on %s:%d, in %s mode', host, port, env)
