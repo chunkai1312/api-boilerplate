361cdc036761aca1021328b8add6ebea73b995aa
Chun-Kai Wang <chunkai1312@gmail.com>
2017-07-21 18:33:36 +0800

    refactor: middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -5,11 +5,12 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import helmet from 'helmet'
 import methodOverride from 'method-override'
+import addRequestId from 'express-request-id'
 import { compose } from 'compose-middleware'
 import expressWinston from 'express-winston'
 import config from '../config'
 import { accessLogger as winstonInstance } from '../config/winston'
-import addRequestId from 'express-request-id'
+import errorHandler from './errorhandler'
 
 const logger = (config.env === 'production')
   ? () => expressWinston.logger({ winstonInstance, dynamicMeta: (req, res) => ({ requestId: req.id }) })
@@ -28,4 +29,4 @@ const middlewares = [
 ]
 
 export default () => compose(middlewares)
-export errorHandler from './errorhandler'
+export { errorHandler }
