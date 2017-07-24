fe4a4d90585c3dfcfcaaaa9a381d47e1d19a2c2a
Chun-Kai Wang <chunkai1312@gmail.com>
2017-07-24 11:10:59 +0800

    refactor: middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -9,9 +9,9 @@ import requestId from 'express-request-id'
 import responseTime from 'response-time'
 import { compose } from 'compose-middleware'
 import expressWinston from 'express-winston'
-import config from '../config'
-import { accessLogger as winstonInstance } from '../config/winston'
 import errorHandler from './errorHandler'
+import { accessLogger as winstonInstance } from '../config/winston'
+import config from '../config'
 
 const logger = (config.env === 'production')
   ? () => expressWinston.logger({ winstonInstance, dynamicMeta: (req, res) => ({ requestId: req.id }) })
