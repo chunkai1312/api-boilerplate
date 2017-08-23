19d2bade68510183e11977aa7ae06211d15cf3a4
Chun-Kai Wang <chunkai1312@gmail.com>
2017-08-23 11:59:23 +0800

    chore: jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testRegex: '(/test/.*\\.spec.js)$',
+  testEnvironment: 'node',
+  setupFiles: [
+    '<rootDir>/test/setup.js'
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/test',
+    '<rootDir>/src/bootstrap/',
+    '<rootDir>/src/config/',
+    '<rootDir>/src/database',
+    '<rootDir>/src/lib',
+    '<rootDir>/src/app/middlewares/index.js',
+    '<rootDir>/src/app/middlewares/errors/index.js'
+  ],
+  coverageReporters: ['text', 'text-summary']
+}
